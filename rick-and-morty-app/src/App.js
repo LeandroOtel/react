d2037a7afe5c761581deeb2beb7a027fbbdcf2c7
Leandro <leandrootel2705@gmail.com>
2022-08-04 23:27:02 -0300

    terminado

--- rick-and-morty-app/src/App.js
+++ rick-and-morty-app/src/App.js
@@ -1,7 +1,53 @@
+import React, { Component, useEffect, useState } from 'react'
 import Navbar from "./components/Navbar";
+import Characters from "./components/Characters"
+import Paginacion from './components/Paginacion';
+
 function App() {
-  return (
-    <Navbar brand="rick and morty app"/>
+
+  const [characters, setCharacters] = useState([]);
+
+  const [info, setInfo] = useState({});
+
+  const initialUrl = "https://rickandmortyapi.com/api/character"
+
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => { setCharacters(data.results); setInfo(data.info) })
+      .catch(error => console.log(error))
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
+
+  useEffect(() => {
+
+    fetchCharacters(initialUrl);
+  }, [])
+
+
+  return (<>
+    <Navbar brand="rick and morty app" />
+    <div className='container mt-5'>
+      <Paginacion
+        prev={info.prev}
+        next={info.next}
+        onPrevious={onPrevious}
+        onNext={onNext} />
+      <Characters characters={characters}/>
+      <Paginacion
+        prev={info.prev}
+        next={info.next}
+        onPrevious={onPrevious}
+        onNext={onNext} />
+    </div>
+  </>
   );
 }
 
